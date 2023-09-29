418abf435322893a9aac8a689a10751fbff4c62b
yana <vasyana750@gmail.com>
2023-09-29 16:37:15 +0700

    third

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
-import asyncio
-from aiogram import Bot, Dispatcher
-from aiogram.types import Message
-from aiogram.filters import CommandStart
+import aiogram
+from aiogram import Bot, Dispatcher, types
+from aiogram import executor
 
 from config import TOKEN
 
@@ -9,12 +8,12 @@ bot = Bot(token = TOKEN)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
-async def process_start_command(message: type.Message):
-    await message.reply("Привет!\Напиши мне что-нибудь!")
+async def process_start_command(message: types.Message):
+    await message.reply("Привет!\nНапиши мне что-нибудь!")
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
-    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+    await message.reply("Напиши мне что-нибудь, и я отправлю этот текст тебе в ответ!")
 
 @dp.message_handler()
 async def echo_message(msg: types.Message):
