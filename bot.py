ff07f79cf71d61f4c2bb81c78813abec1dca2c26
yana <vasyana750@gmail.com>
2023-10-04 21:12:14 +0700

    six

--- bot.py
+++ bot.py
@@ -1,80 +1,61 @@
+import asyncio
 import logging
-import aiogram
+import sys
+from os import getenv
+from typing import Any, Dict
 
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram import Bot, Dispatcher, F, Router, html
+from aiogram.enums import ParseMode
+from aiogram.filters import Command, CommandStart
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.types import (
+    KeyboardButton,
+    Message,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+)
+
 
 from config import TOKEN
-from utils1 import TestStates
-from messages import MESSAGES
 
-bot = Bot(token = TOKEN)
-dp = Dispatcher(bot, storage=MemoryStorage())
+#logging.basicConfig(format=u'%(Aiogramm)+13s [ LINE:%(lineno)-4s] %(levelname)-8s [%(asctime)s] %(message)s',
+                    #level=logging.DEBUG)
 
-dp.middleware.setup(LoggingMiddleware())
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
 
-@dp.message_handler(commands=['start'])
-async def process_start_command(message: types.Message):
-    await message.reply(MESSAGES['start'])
+class Form(StatesGroup):
+    name = State()
+    like_bots = State()
 
+@Form.message_handler(commands = ['start'])
+async def process_start_command(message: Message, state: FSMContext) -> None:
+    await state.set_state(Form.name)
+    await message.answer(
+        "Привет! \n Как вас зовут?",
+        reply_markup=ReplyKeyboardRemove(),
+    )
 
-@dp.message_handler(commands=['help'])
-async def process_help_command(message: types.Message):
-    await message.reply(MESSAGES['help'])
+@Form.message_handler(Form.name)
+async def process_name(message: Message, state: FSMContext) -> None:
+    await state.update_data(name=message.text)
+    await state.set_state(Form.like_bots)
+    await message.answer(
+        f"Приятно познакомится, {html.quote(message.text)}!\n По какой группе ищете информацию?",
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text="Yes"),
+                    KeyboardButton(text="No"),
+                ]
+            ],
+            resize_keyboard=True,
+        ),
+    )
 
-
-@dp.message_handler(state='*', commands=['setstate'])
-async def process_setstate_command(message: types.Message):
-    argument = message.get_args()
-    state = dp.current_state(user=message.from_user.id)
-    if not argument:
-        await state.reset_state()
-        return await message.reply(MESSAGES['state_reset'])
-
-    if (not argument.isdigit()) or (not int(argument) < len(TestStates.all())):
-        return await message.reply(MESSAGES['invalid_key'].format(key=argument))
-
-    await state.set_state(TestStates.all()[int(argument)])
-    await message.reply(MESSAGES['state_change'], reply=False)
-
-
-@dp.message_handler(state=TestStates.TEST_STATE_1)
-async def first_test_state_case_met(message: types.Message):
-    await message.reply('Первый!', reply=False)
-
-
-@dp.message_handler(state=TestStates.TEST_STATE_2[0])
-async def second_test_state_case_met(message: types.Message):
-    await message.reply('Второй!', reply=False)
-
-
-@dp.message_handler(state=TestStates.TEST_STATE_3 | TestStates.TEST_STATE_4)
-async def third_or_fourth_test_state_case_met(message: types.Message):
-    await message.reply('Третий или четвертый!', reply=False)
-
-
-@dp.message_handler(state=TestStates.all())
-async def some_test_state_case_met(message: types.Message):
-    with dp.current_state(user=message.from_user.id) as state:
-        text = MESSAGES['current_state'].format(
-            current_state=await state.get_state(),
-            states=TestStates.all()
-        )
-    await message.reply(text, reply=False)
-
-
-@dp.message_handler()
-async def echo_message(msg: types.Message):
-    await bot.send_message(msg.from_user.id, msg.text)
-
-
-async def shutdown(dispatcher: Dispatcher):
-    await dispatcher.storage.close()
-    await dispatcher.storage.wait_closed()
-
+async def main():
+   await dp.start_polling(bot)
 
 if __name__ == '__main__':
-    executor.start_polling(dp, on_shutdown=shutdown)
+    asyncio.run(main())
